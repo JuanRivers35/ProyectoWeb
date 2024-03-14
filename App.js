2477e8698d908297646cfa0f324e799e5b48c86e
JuanRivers35 <161341940+JuanRivers35@users.noreply.github.com>
2024-03-14 15:52:14 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import logo from './snowflake.jpg';
+import './App.css';
+import { Form, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Form.Label>Nombre : </Form.Label>
+        <Form.Control type="text"></Form.Control>
+        <Button>Guardar</Button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
