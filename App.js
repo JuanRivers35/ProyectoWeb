d7f32ff711ee83533dac6bb6a7cd44e36d741f29
JuanRivers35 <161341940+JuanRivers35@users.noreply.github.com>
2024-03-15 17:49:10 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,35 @@
-import logo from './snowflake.jpg';
+import {useState} from 'react';
+import logo from './snowflake.jpg'
 import './App.css';
-import { Form, Button } from 'react-bootstrap';
+import PrimerFormulario from './Componentes2/PrimerFormulario';
+import {Button, Form}  from 'react-bootstrap';
+
 
 function App() {
+const [personaNombre,setPersona] = useState("Juan");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Form.Label>Nombre : </Form.Label>
-        <Form.Control type="text"></Form.Control>
-        <Button>Guardar</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          wato
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+       <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
         >
-          Learn React
+         Learn React
         </a>
-        <Button variant = "primary" > wato </Button> {''}
+        <PrimerFormulario personaNombre={personaNombre} />
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+
