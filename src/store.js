946320329f66299bed2ee3986ff9c6c71eb15327
juanchi <161341940+JuanRivers35@users.noreply.github.com>
2024-06-03 13:27:50 -0700

    Delete src/store.js

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { getUsersReducer } from "../slice/userSlice";
-
-export default configureStore({
-    reducer:{
-        getUsers : getUsersReducer
-    },
-    middleware: (getDefultMiddleware) => getDefultMiddleware({ serializableCheck: false})
-});
