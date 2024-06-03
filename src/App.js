f9f63ae47e88ecb0673eddd0ac1b100608bc1265
juanchi <161341940+JuanRivers35@users.noreply.github.com>
2024-06-03 14:11:34 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import { useState } from 'react';
-import logo from './logo.svg';
+import logo from './snowflake.jpg';
 import './App.css';
 // import PrimerFormulario from './componentes/PrimerFormulario';
 
